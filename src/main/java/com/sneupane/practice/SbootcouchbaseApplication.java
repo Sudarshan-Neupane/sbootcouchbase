1836fada0df1dae8520232981757ce7dad8ed95b
Sudarshan Neupane <mesudarshan7@gmail.com>
2017-01-12 23:14:23 -0500

    add object on main class

--- src/main/java/com/sneupane/practice/SbootcouchbaseApplication.java
+++ src/main/java/com/sneupane/practice/SbootcouchbaseApplication.java
@@ -1,13 +1,30 @@
 package com.sneupane.practice;
 
+import com.sneupane.practice.model.Person;
+import com.sneupane.practice.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
 public class SbootcouchbaseApplication {
+	@Autowired
+	private PersonRepository personRepo;
 
 	public static void main(String[] args) {
-		System.out.println(" Systout " );
+		SbootcouchbaseApplication cp = new SbootcouchbaseApplication();
+		Person saveperson = cp.savePerson();
 		SpringApplication.run(SbootcouchbaseApplication.class, args);
+	}
+
+	private Person savePerson(){
+		Person person = new Person();
+		person.setCreated(new Date());
+		person.setFirstName("Sudarshan Neupane");
+		person.setId("12345678");
+		return this.personRepo.save(person);
+
 	}
 }
